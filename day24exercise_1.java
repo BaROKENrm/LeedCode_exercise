0505c4401db7a303f06fe106d2573318f2ff9a02
BaROKENrm <1336400335@qq.com>
2020-12-26 22:50:56 +0800

    同构字符串

---
+++ day24exercise_1.java
@@ -0,0 +1,23 @@
+package day24;
+//        给定两个字符串 s 和 t，判断它们是否是同构的。
+//        如果 s 中的字符可以被替换得到 t ，那么这两个字符串是同构的。
+//        所有出现的字符都必须用另一个字符替换，同时保留字符的顺序。两个字符不能映射到同一个字符上，但字符可以映射自己本身。
+
+
+public class day24exercise_1 {
+    public boolean isIsomorphic(String s, String t) {
+        char[] chars = s.toCharArray();
+        char[] chart = t.toCharArray();
+        int[] preIndexOfs = new int[256];
+        int[] preIndexOft = new int[256];
+        for (int i = 0; i < chars.length; i++) {
+            if (preIndexOfs[chars[i]] != preIndexOft[chart[i]]) {
+                return false;
+            }
+            preIndexOfs[chars[i]] = i + 1;
+            preIndexOft[chart[i]] = i + 1;
+        }
+        return true;
+    }
+}
+
