87f60d7543a059cddf6a673b6edde78d5700ee71
BaROKENrm <1336400335@qq.com>
2020-12-31 16:49:05 +0800

    走路问题

---
+++ day28exercise_2.java
@@ -0,0 +1,17 @@
+package day28;
+//一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+//
+//        机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+//
+//        问总共有多少条不同的路径？
+
+
+public class day28exercise_2 {
+    public int uniquePaths(int m, int n) {
+        long ans = 1;
+        for (int x = n, y = 1; y < m; ++x, ++y) {
+            ans = ans * x / y;
+        }
+        return (int) ans;
+    }
+}
