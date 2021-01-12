79f5b469c287f1d44ebb84305081661ed6f2d632
BaROKENrm <1336400335@qq.com>
2021-01-12 23:34:12 +0800

    动态规划

---
+++ day37exercise_2.java
@@ -0,0 +1,25 @@
+package day37;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+//给定一个非空字符串 s 和一个包含非空单词的列表 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
+public class day37exercise_2 {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>(wordDict);
+        int len = s.length();
+        boolean[] dp = new boolean[len + 1];
+        dp[0] = true;
+
+        for (int i = 0; i <= len; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && set.contains(s.substring(j,i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[len];
+    }
+}
