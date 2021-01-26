d3f3e9d23ef4b4d9df1f51833a73b3fbde4b8c52
BaROKENrm <1336400335@qq.com>
2021-01-26 21:50:14 +0800

    bellmanFord算法

---
+++ day48exercise_1.java
@@ -0,0 +1,74 @@
+package day48;
+//给定一个n个点m条边的有向图，图中可能存在重边和自环， 边权可能为负数。
+//
+//        请你求出从1号点到n号点的最多经过k条边的最短距离，如果无法从1号点走到n号点，输出impossible。
+//
+//        注意：图中可能 存在负权回路 。
+//
+//        输入格式
+//        第一行包含三个整数n，m，k。
+//
+//        接下来m行，每行包含三个整数x，y，z，表示存在一条从点x到点y的有向边，边长为z。
+//
+//        输出格式
+//        输出一个整数，表示从1号点到n号点的最多经过k条边的最短距离。
+//
+//        如果不存在满足条件的路径，则输出“impossible”。
+//
+//        数据范围
+//        1≤n,k≤500,
+//        1≤m≤10000,
+//        任意边长的绝对值不超过10000。
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class day48exercise_1 {
+    static class Edge {//从a走向b的边 权重是w
+        int a, b, w;
+        public Edge(int a, int b, int w){
+            this.a = a; this.b = b; this.w = w;
+        }
+    }
+
+    static int N = 10010, max = 0x3f3f3f3f;
+    static int m, n, k;
+    static int[] dist = new int[N];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt(); m = sc.nextInt(); k = sc.nextInt();
+
+        Edge[] edge = new Edge[N];
+        for (int i = 1; i <= m; i++) {
+            int a = sc.nextInt(), b = sc.nextInt(), w = sc.nextInt();
+            edge[i] = new Edge(a, b, w);
+        }
+
+        int t = bellmanFord(edge);
+        if (t == -1) System.out.println("impossible");
+        else System.out.println(t);
+    }
+
+    private static int bellmanFord(Edge[] edge) {
+        Arrays.fill(dist, max);
+        dist[1] = 0;
+
+        // 进行 k 次 松弛操作
+        for(int j = k;k > 0;k--) {
+            //每次对dist数组进行一次备份
+            //备份原因：每次只会从上一轮的结果里面去改动，不会被本轮改动的值所影响
+            int[] copy = Arrays.copyOf(dist, N);
+            //对每个边进行遍历，找到长度为k的最短距离
+            for (int i = 1; i <= m; i++) {
+                int a = edge[i].a, b = edge[i].b, w = edge[i].w;
+                dist[b] = Math.min(copy[a] + w, dist[b]);
+            }
+        }
+
+        // 为了防止出现从1到n其实没有路径，但是可以从max通过一个负值进行更新的情况
+        if (dist[n] > max / 2) return -1;
+        else return dist[n];
+    }
+}
+
